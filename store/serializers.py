23597e513bdcf32c37c047e85bf7236fa1ddbf34
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2024-08-30 13:51:49 -0500

    Update

--- store/serializers.py
+++ store/serializers.py
@@ -4,7 +4,7 @@ from .models import Product,Category
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id','name','category','Balance','Title','Info','slug','price','Status')
+        fields = '__all__'
         
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
