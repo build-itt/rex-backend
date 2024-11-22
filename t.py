0741efcd9591d97a723188b787a7907e98866f28
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2024-11-22 19:27:48 +0000

    update to media

--- t.py
+++ t.py
@@ -1,9 +1,14 @@
-import smtplib
+import boto3
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id='0050e63d27947900000000006',
+    aws_secret_access_key='K005WJN78p8Y98f4nB7LLFSNUqr7moY',
+    endpoint_url='https://s3.us-east-005.backblazeb2.com'
+)
 
 try:
-    server = smtplib.SMTP('127.0.0.1', 1025)
-    server.starttls()
-    server.login('darkpass45@proton.me', 'acLN8l9oZNlLdCTFZrJthQ')
-    print("Connection successful")
+    response = s3.list_buckets()
+    print("Buckets:", response['Buckets'])
 except Exception as e:
-    print(f"Failed to connect: {e}")
+    print("Error:", e)
